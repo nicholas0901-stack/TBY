4e34cd24c692c519020b2c3a67284a106d3cb6b1
nicholas0901-stack <60474345+nicholas0901-stack@users.noreply.github.com>
2023-04-22 17:01:01 +0800

    Update Card.java

--- Card.java
+++ Card.java
@@ -1,31 +1,57 @@
-;
+
 public class Card {
 	// variable
 	private String suit;
 	private int rank;
 	private int value;
 	private String[] rankNames = { "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King" };
+	public enum SuitRank { CLUBS, DIAMONDS, HEARTS, SPADES;
+	private SuitRank suits;
+	
+	}
 	// Constructors
 	public Card(String suit, int rank) {
 		this.suit = suit;
 		this.rank = rank;
 		if (rank == 1) {
-			this.value = 1; // Ace = value of 1
-		} else if (rank > 1 && rank <= 10) {
-			this.value = rank; // determine that face card has it own faced value from 2 - 9
+		    this.value = 1; // Ace = value of 1
+		} else if (rank >= 2 && rank <= 10) {
+		    this.value = rank; // determine that face card has it own faced value from 2 - 10
+		} else if (rank >= 11 && rank <= 13) {
+		    this.value = 10; // Jack, Queen, and King valued at 10
 		} else {
-			this.value = 10; // determine Jack, Queen , King valued at 10
+		    this.value = 0; // invalid rank
 		}
 	}
 
-	public Card(int value, String suit) {
+	public Card(int value, String suit, int rank) {
 		this.value = value;
 		this.suit = suit;
-	}
+        this.rank = rank;
+
+        switch (suit) {
+            case "Spades":
+                this.value = 4;
+                break;
+            case "Hearts":
+                this.value = 3;
+                break;
+            case "Diamonds":
+                this.value = 2;
+                break;
+            case "Clubs":
+                this.value = 1;
+                break;
+            default:
+                throw new IllegalArgumentException("Invalid suit: " + suit);
+        }
+    }
+	
 
 	// getter and setter
 	public String getSuit() {
 		return suit;
+		
 	}
 
 	public void setSuit(String suit) {
@@ -34,6 +60,7 @@ public class Card {
 
 	public int getRank() {
 		return rank;
+		
 	}
 
 	public void setRank(int rank) {
@@ -53,20 +80,27 @@ public class Card {
 		String rankName = rankNames[rank - 1];
 		return " <" + suit + " " + rankName + "> ";
 	}
-	public enum SuitRank {
-		
-	    CLUBS(1),
-	    DIAMONDS(2),
-	    HEARTS(3),
-	    SPADES(4);
-		private final int rank;
-	    private SuitRank(int rank) {
-	        this.rank = rank;
+	public SuitRank getSuitRank() {
+	    switch (suit) {
+	        case "clubs":
+	            return SuitRank.CLUBS;
+	        case "diamonds":
+	            return SuitRank.DIAMONDS;
+	        case "hearts":
+	            return SuitRank.HEARTS;
+	        case "spades":
+	            return SuitRank.SPADES;
+	        default:
+	            return null;}
 	    }
+	
 
-	    public int getRank() {
-	        return rank;
-	    }
-	}
-
+	        // compare the suits based on the priority given in the prompt
+	   
+	public int compareSuit(Card otherCard) {
+        return Integer.compare(this.value, otherCard.value);
+    }
+	    
 }
+
+	    
