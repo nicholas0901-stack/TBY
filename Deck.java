e268e22f40e7e7216a865528392348f0ae41a7e0
nicholas0901-stack <60474345+nicholas0901-stack@users.noreply.github.com>
2023-04-23 23:51:07 +0800

    Update Deck.java

--- Deck.java
+++ Deck.java
@@ -1,35 +1,38 @@
-import java.util.ArrayList;
-import java.util.Collections;
 
-public class Deck {
-	private ArrayList<Card> cards;
-
-	public Deck() {
-		cards = new ArrayList<Card>();
-		for (int rank = 1; rank <= 13; rank++) {
-			for (String suit : new String[] { "Spade", "Heart", "Diamond", "Club" }) {
-				Card card = new Card(suit, rank);
-				cards.add(card);
-			}
-		}
-	}
-
-	// method to shuffle card
-	public void shuffle() {
-		Collections.shuffle(cards);
-	}
-
-	// method to draw card
-	public Card drawCard() {
-		if (cards.isEmpty()) {
-			return null;
-		}
-		return cards.remove(0);
-	}
-
-	// method to to return card size
-	public int size() {
-		return cards.size();
-	}
-	
+import java.util.ArrayList; 
+import java.util.Collections; 
+ 
+public class Deck { 
+ private ArrayList<Card> cards; 
+ private final int DECK_SIZE = 52;
+ 
+ public Deck() { 
+	 
+  cards = new ArrayList<Card>(DECK_SIZE); 
+  for (int rank = 1; rank <= 13; rank++) { 
+   for (String suit : new String[] { "SPADES", "HEARTS", "DIAMONDS", "CLUBS" }) { 
+    Card card = new Card(suit, rank); 
+    cards.add(card); 
+   } 
+  } 
+ } 
+ 
+ // method to shuffle card 
+ public void shuffle() { 
+  Collections.shuffle(cards); 
+ } 
+ 
+ // method to draw card 
+ public Card drawCard() { 
+  if (cards.isEmpty()) { 
+   return null; 
+  } 
+  return cards.remove(0); 
+ } 
+ 
+ // method to to return card size 
+ public int size() { 
+  return cards.size(); 
+ } 
+  
 }
